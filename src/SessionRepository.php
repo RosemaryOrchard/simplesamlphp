2113958af990a7cf7024ed9e587a76470386bfca
RosemaryOrchard <16113535+RosemaryOrchard@users.noreply.github.com>
2020-02-26 16:06:50 +0100

    Update repository to add a function to remove expired sessions

--- src/SessionRepository.php
+++ src/SessionRepository.php
@@ -4,4 +4,15 @@ use Doctrine\ORM\EntityRepository;
 
 class SessionRepository extends EntityRepository
 {
+    public function removeExpiredSessions()
+    {
+        $expired = $this
+            ->getEntityManager()
+            ->getRepository(Session::class)
+            ->findBy(['expiresAt' => new \DateTime()]);
+        foreach ($expired as $record) {
+            $this->getEntityManager()->remove($record);
+        }
+        $this->getEntityManager()->flush();
+    }
 }
